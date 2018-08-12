ef7b6f041bed3a4727db4f12905800677c58b365
Ville Väänänen <ville.vaananen@lucify.com>
2018-08-12 20:20:23 +0300

    Allow using GATSBY_* env variables

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,10 +1,19 @@
 import { CognitoAuth } from 'amazon-cognito-auth-js';
 
 function getCognitoAuthOptions() {
-  const cognitoDomain = process.env.COGNITO_DOMAIN;
-  const cognitoUserPoolClientId = process.env.COGNITO_USER_POOL_CLIENT_ID;
-  const externalBaseUrl =
-    process.env.EXTERNAL_BASE_URL || 'http://localhost:8000';
+  const keys = [
+    'COGNITO_DOMAIN',
+    'COGNITO_USER_POOL_CLIENT_ID',
+    'EXTERNAL_BASE_URL',
+  ];
+  const defaults = [undefined, undefined, 'http://localhost:8000'];
+  const values = keys.map(
+    (k,i) => process.env[k] || process.env[`GATSBY_${k}`] || defaults[i]
+  );
+  if (values.reduce((s, v) => v ? s + 1 : s, 0) !== keys.length) {
+    throw new Error('Required auth variables missing');
+  }
+  const [cognitoDomain, cognitoUserPoolClientId, externalBaseUrl] = values;
   const signinRedirectUrl = `${externalBaseUrl}/signin`;
   const signoutRedirectUrl = `${externalBaseUrl}/signout`;
   const responseType = 'token';
