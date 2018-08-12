2fa6c5ac0c715bcc32b25d88c0da7b284db1a08a
Ville Väänänen <ville.vaananen@lucify.com>
2018-08-12 20:27:09 +0300

    Include appsync config

---
+++ src/appsync/config.ts
@@ -0,0 +1,14 @@
+﻿import { AUTH_TYPE, AWSAppSyncClientOptions } from 'aws-appsync';
+import { AuthOptions } from 'aws-appsync/lib/link/auth-link';
+
+export const AppSyncConf = (auth: AuthOptions): AWSAppSyncClientOptions => {
+  return {
+    url:
+      'https://ugjxe2tdzzhsjfuocm3fnzbi4y.appsync-api.eu-west-1.amazonaws.com/graphql',
+    region: 'eu-west-1',
+    auth,
+    complexObjectsCredentials: () => null,
+  };
+};
+
+
