50a43edfdebaf2e331eb7a5c3fb18cdafb7b9af9
Ville Väänänen <vjvaanan@gmail.com>
2018-10-07 14:45:00 +0300

    Update everything

--- gatsby-node.js
+++ gatsby-node.js
@@ -19,7 +19,7 @@ exports.onCreatePage = ({ page, actions }) => {
   const { createPage } = actions
   // Make the front page match everything client side.
   // Normally your paths should be a bit more judicious.
-  console.log(page.path);
+  // console.log(page.path);
   if (page.path === `/`) {
     page.matchPath = `/*`
     createPage(page)
