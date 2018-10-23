3ec8073c2fdbca3e029c2f1f7308cacb2282882c
Ville Väänänen <vjvaanan@gmail.com>
2018-10-23 22:12:09 +0300

    Rename files to get over case-sensitivity problems

--- src/components/sidebar-right.tsx
+++ src/components/sidebar-right.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import { Tabs, Tab, Icon, Button, Divider } from '@blueprintjs/core';
 import { colors, mobileOnly } from '../theme/theme';
 import { connect } from '../utils';
-import { Properties } from './properties';
+import { Properties } from './properties-form';
 interface ContainerProps {
   show?: boolean;
 }
