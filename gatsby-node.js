83d07277e75c47fb76dcb07803dc8f806e3c3993
Ville Väänänen <vjvaanan@gmail.com>
2019-01-04 20:51:07 +0200

    Fix Gatsby build issues

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,7 @@
+const path = require('path');
 exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
-  if (stage === "build-html") {
+  if (stage === 'build-html') {
+    console.log(loaders.null());
     actions.setWebpackConfig({
       module: {
         rules: [
@@ -7,21 +9,25 @@ exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
             test: /mobx-react-devtools/,
             use: loaders.null(),
           },
+          {
+            test: /.*\/codemirror\/.*/,
+            use: loaders.null(),
+          },
         ],
       },
-    })
+    });
   }
-}
+};
 
 // Implement the Gatsby API “onCreatePage”. This is
 // called after every page is created.
 exports.onCreatePage = ({ page, actions }) => {
-  const { createPage } = actions
+  const { createPage } = actions;
   // Make the front page match everything client side.
   // Normally your paths should be a bit more judicious.
   // console.log(page.path);
   if (page.path === `/`) {
-    page.matchPath = `/*`
-    createPage(page)
+    page.matchPath = `/*`;
+    createPage(page);
   }
-}
+};
--- src/utils/splitter.tsx
+++ src/utils/splitter.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Split from 'split.js';
+import Split from '../utils/split';
 import styled from 'styled-components';
 
 const Container = styled.div`
